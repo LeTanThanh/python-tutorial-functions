9b87d621a20b9be2bccf1d7912a32aee562c8cbf
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-28 11:52:08 +0700

    Passing information to Python functions

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 if __name__ == "__main__":
   # Defining a Python function
 
-  def greet():
-    """Display a greeting a users"""
-    print("Hi")
+  # def greet():
+  #   """Display a greeting a users"""
+  #   print("Hi")
 
-  greet()
+  # greet()
+
+  # Passing information to Python functions
+
+  def greet(name):
+    print(f"Hi {name}")
+
+  greet("John")
+
+  first_name = "Jane"
+  greet(first_name)
